8ef18c32e70f129655856fb95f82368ffad1961e
nikita-hire <65800164+nikita-hire@users.noreply.github.com>
2021-02-23 11:42:33 +0530

    Create PrintAtoZ.java

---
+++ PrintAtoZ.java
@@ -0,0 +1,19 @@
+class PrintAtoZ
+{
+	public static void main(String args[])
+	{
+		char c;
+		System.out.println("\n");
+		for(c='A';c<='Z';++c)
+		{
+			System.out.print(c+" ");
+		}	
+		System.out.println("\n");
+		for(c='a';c<='z';++c)
+		{
+			System.out.print(c+" ");
+		}
+		System.out.println("\n");
+		
+	}
+}
